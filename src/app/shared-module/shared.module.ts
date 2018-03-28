da0123c173789e22b76f67d2c800174e59238d92
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2018-03-28 15:52:01 +0200

    asdfas

---
+++ src/app/shared-module/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent //ręczny export komponentów, po to żeby inne moduły importujace ten moduł widziały te komponenty
+  ],
+  declarations: [HeaderComponent]
+})
+export class SharedModule { }
