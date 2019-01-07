f9ad0ac8fc1cfa2c1e46b7578ed7f2d245c74d66
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2019-01-07 11:39:12 +0100

    add host listener

--- src/app/cars/cars-list-row/cars-list-row.component.ts
+++ src/app/cars/cars-list-row/cars-list-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, HostBinding, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostBinding, OnInit, HostListener, ElementRef, Renderer2 } from '@angular/core';
 import { Car } from '../models/car';
 
 @Component({
@@ -11,10 +11,26 @@ export class CarsListRowComponent implements OnInit{
   @Output() removedCar = new EventEmitter();
   // tslint:disable-next-line:max-line-length
   @HostBinding('class.after-deadline') deadline = false; // steruje czy podpiąć klasę (komponent sam decyduje o tym - zamiast użycia [ngClass] w nadrzędnym komponencie)
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setRemoveBtnStyle('#f00');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setRemoveBtnStyle('#000');
+  }
+
+
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) { }
 
   public removeCar(car, event) {
     event.stopPropagation();
     this.removedCar.emit(car);
+  }
+
+  private setRemoveBtnStyle(color: string) {
+    this.renderer.setStyle(this.element.nativeElement.querySelector('.remove-btn'), 'color', color);
   }
 
   ngOnInit() {
