214430c792f0bfde534bdd8d6551916db23be81a
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2019-08-08 10:01:11 +0200

    add shallow pipe test

--- src/app/shared-module/pipes/user-shortcut.pipe.spec.ts
+++ src/app/shared-module/pipes/user-shortcut.pipe.spec.ts
@@ -1,14 +1,55 @@
 import { UserShortcutPipe } from './user-shortcut.pipe';
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 
 describe('UserShortcutPipe', () => {
 
-  const pipe = new UserShortcutPipe();
+  describe('Shallow UserShortcutPipe test', () => {
+    @Component({
+      template: `
+        Username: {{ username | userShortcut }}
+      `
+    })
+    class TestComponent {
+      username = 'Mateusz';
+    }
 
-  it('should return first letter and dot', () => {
-    expect(pipe.transform('Mateusz')).toBe('M.');
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          UserShortcutPipe,
+          TestComponent
+        ]
+      });
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('should return first letter and dot', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Username: M.');
+      component.username = 'Andzej';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Username: A.');
+    });
+
   });
 
-  it('should return empty string', () => {
-    expect(pipe.transform('')).toBe('');
+  describe('Isolate UserShortcutPipe test', () => {
+    const pipe = new UserShortcutPipe();
+
+    it('should return first letter and dot', () => {
+      expect(pipe.transform('Mateusz')).toBe('M.');
+    });
+
+    it('should return empty string', () => {
+      expect(pipe.transform('')).toBe('');
+    });
   });
+
 });
